cd4f68ad8dbdeaefb7119c2c0b14c5f0c6477820
knightSarai <knight.sarai.dev@gmail.com>
2021-01-18 19:13:02 +0200

    Add custom manager

--- main/models.py
+++ main/models.py
@@ -4,10 +4,18 @@ from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
 
 
+class PublishedManager(models.Manager):
+
+    def get_queryset(self):
+        return super(PublishedManager, self) \
+            .get_queryset() \
+            .filter(status='Published')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
-        ('Published', 'Published')
+        ('published', 'Published')
     )
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
@@ -17,6 +25,9 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ('-publish',)
