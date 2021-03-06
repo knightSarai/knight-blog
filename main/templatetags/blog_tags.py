12bd5fcc35b1dc547786273c567ee064ea557739
knightSarai <knight.sarai.dev@gmail.com>
2021-03-06 23:36:34 +0200

    Commit prev files

--- main/templatetags/blog_tags.py
+++ main/templatetags/blog_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.db.models import Count
+
 from ..models import Post
 
 register = template.Library()
@@ -7,3 +9,17 @@ register = template.Library()
 @register.simple_tag
 def total_posts():
     return Post.published.count()
+
+
+@register.inclusion_tag('post/latest_posts.html')
+def show_latest_posts(count=3):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=3):
+    return Post.published.annotate(
+        total_comments=Count('comments')
+    ).order_by('-total_comments')[:count]
+
